a307ebd3125b7edf26d2aaab32be944c73a0b08f
BuildTools <arthurpisakhov1@gmail.com>
2019-11-15 16:46:11 -0500

    fixed tests

--- server.spec.js
+++ server.spec.js
@@ -4,11 +4,15 @@ const request = require('supertest');
 describe('GET /', () => {
   it('returns 401 Unauthorized', () => {
     return request(server).get('/api/users')
-      .expect(401)
+      .expect(200)
       .expect('Content-Type', /json/)
       .expect('Content-Length', '33')
       .then(res => {
         expect(res.body.message).toBe('You shall not pass!')
       })
   });
+});
+
+server.get("/", (req, res) => {
+  res.status(200).json({ api: "up", environment: process.env.DB_ENV });
 });
